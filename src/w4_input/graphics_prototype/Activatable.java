684cf5abaedac8e9eca25eaa261322a06a77cd98
martin <martin@localhost>
2017-07-30 18:25:25 +0200

    Interface for everything activatable

---
+++ src/w4_input/graphics_prototype/Activatable.java
@@ -0,0 +1,17 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package w4_input.graphics_prototype;
+
+/**
+ *
+ * @author martin
+ */
+public interface Activatable {
+
+public void activate();
+
+public void reset();
+}
